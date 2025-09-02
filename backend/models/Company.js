8d95aa18f1c71abc85e518a64be79ced9e594942
Manthankakkar <Manthankakkar92555@gmail.com>
2025-09-02 12:25:13 +0530

    changed user model

--- backend/models/Company.js
+++ backend/models/Company.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require("../config/db");
-const User=require("../models/user")
+const User=require("./user")
 
 const Company = sequelize.define("Company", {
   name: {
